95e914cfcb218fc2a19da739ea46e9e9eb5b40fc
Ved Dandekar <veddandekar6@gmail.com>
2021-05-03 15:50:33 +0530

    Updates

---
+++ precache-manifest.d1809b0144d35f49cad07008ac7229a8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f933ae81e3b0b012fec706b133fa450",
+    "url": "/CoinTrace/index.html"
+  },
+  {
+    "revision": "d24dd9070752163c03c9",
+    "url": "/CoinTrace/static/css/main.902c00fd.chunk.css"
+  },
+  {
+    "revision": "b9fcdf37d0b9fe8de04d",
+    "url": "/CoinTrace/static/js/0.35ce4d45.chunk.js"
+  },
+  {
+    "revision": "2ec2d644459c6c9e3297",
+    "url": "/CoinTrace/static/js/1.38656e6e.chunk.js"
+  },
+  {
+    "revision": "9312913abd3ec291e09a77758d4b839e",
+    "url": "/CoinTrace/static/js/1.38656e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dddcda4d3c5c755c601",
+    "url": "/CoinTrace/static/js/10.b40191da.chunk.js"
+  },
+  {
+    "revision": "8ef055d88617fedfa4dd",
+    "url": "/CoinTrace/static/js/11.4836d8c8.chunk.js"
+  },
+  {
+    "revision": "47d30f6ae55b0e2a0a03",
+    "url": "/CoinTrace/static/js/12.26c2a8ac.chunk.js"
+  },
+  {
+    "revision": "ae1dbdeccace56cd053f",
+    "url": "/CoinTrace/static/js/13.b4bf0bb5.chunk.js"
+  },
+  {
+    "revision": "64abb6a733e772e39038",
+    "url": "/CoinTrace/static/js/4.7a3a6f0f.chunk.js"
+  },
+  {
+    "revision": "aa99534ff0e40f9a33a2268a95400827",
+    "url": "/CoinTrace/static/js/4.7a3a6f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d89c319572f31760b1",
+    "url": "/CoinTrace/static/js/5.b90f8e63.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CoinTrace/static/js/5.b90f8e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be18a9969bc99a315d5",
+    "url": "/CoinTrace/static/js/6.9088fdcb.chunk.js"
+  },
+  {
+    "revision": "3672dd0fdfb954b1a0d1",
+    "url": "/CoinTrace/static/js/7.5dcf6f32.chunk.js"
+  },
+  {
+    "revision": "f2f0e6d8721b76f2c4bf",
+    "url": "/CoinTrace/static/js/8.1db80e7e.chunk.js"
+  },
+  {
+    "revision": "fa0e6da13be508f4bbcd",
+    "url": "/CoinTrace/static/js/9.1ab0caf2.chunk.js"
+  },
+  {
+    "revision": "d24dd9070752163c03c9",
+    "url": "/CoinTrace/static/js/main.69fa8dbc.chunk.js"
+  },
+  {
+    "revision": "9d3429b9040978fa3aab",
+    "url": "/CoinTrace/static/js/runtime-main.afd18b50.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6a94ace0dbf63410cd96e0cef74fa76b.js"
+  "/CoinTrace/precache-manifest.d1809b0144d35f49cad07008ac7229a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CoinTrace/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
